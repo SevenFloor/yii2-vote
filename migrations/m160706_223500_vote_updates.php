175b0a2c2e36b5ccb9dfa9234d69079dc07ca961
Sanakoev Anton <a.sanakoev.ifree@gmail.com>
2017-05-22 14:59:01 +0300

    Migration fix 2

--- migrations/m160706_223500_vote_updates.php
+++ migrations/m160706_223500_vote_updates.php
@@ -10,12 +10,10 @@ class m160706_223500_vote_updates extends Migration
     public function up()
     {
         $this->createIndex('vote_target_user_idx', '{{%vote}}', ['entity', 'target_id', 'user_id'], false);
-        $this->execute('ALTER TABLE vote ALTER COLUMN value TYPE boolean USING CASE value WHEN 0 THEN false ELSE true END');
     }
 
     public function down()
     {
         $this->dropIndex('vote_target_user_idx', '{{%vote}}');
-        $this->alterColumn('{{%vote}}', 'value', $this->smallInteger(1)->notNull());
     }
 }
